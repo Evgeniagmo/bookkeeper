9d59ffcf5ef07add842f0da40e26d95db7ca0db2
Evgeniagmo <antoshkina.ep@phystech.edu>
2023-03-09 16:07:39 +0300

    In sqlite_repository.py: class SQLiteRepository added

---
+++ bookkeeper/repository/sqlite_repository.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+from inspect import get_annotations
+from bookkeeper.repository.abstract_repository import AbstractRepository, T
+from typing import Generic, TypeVar, Protocol, Any
+
+
+
+
+class SQLiteRepository(AbstractRepository[T]):
+    def __init__(self, db_file: str, cls: type) -> None:
+        self.db_file = db_file
+        self.table_name = cls.__name__.lower()
+        self.fields = get_annotations(cls, eval_str=True)
+        self.fields.pop('pk')
+
+    def add(self, obj: T) -> int:
+        names = ', '.join(self.fields.keys())
+        p = ', '.join("?" * len(self.fields))
+        values = [getattr(obj, x) for x in self.fields]
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute('PRAGMA foreign_keys = ON')
+            cur.execute(
+                'SELECT 123'
+                #f'INSERT INTO {self.table_name} ({names}) VALUES ({p})',
+                #values
+            )
+            obj.pk = cur.lastrowid
+        con.close()
+        return obj.pk
+
+    def get(self, pk: int) -> T | None:
+        """ Получить объект по id """
+        pass
+
+
+    def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
+        """
+        Получить все записи по некоторому условию
+        where - условие в виде словаря {'название_поля': значение}
+        если условие не задано (по умолчанию), вернуть все записи
+        """
+        pass
+
+
+    def update(self, obj: T) -> None:
+        """ Обновить данные об объекте. Объект должен содержать поле pk. """
+        pass
+
+
+    def delete(self, pk: int) -> None:
+        """ Удалить запись """
+        pass
+
+# r = SQLiteRepository('test.sqlite', Test)
+# o = Test('Hello')
+# r.add(o)
